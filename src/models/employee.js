1a72b8b81da9a13afedbe853582be4ffee2db440
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-27 17:52:59 +0545

    Employee model

---
+++ src/models/employee.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const EmployeeSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model('Employee', EmployeeSchema)
