e26bc1ef46ea446f9c997590cd13c24931b9f108
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-27 12:21:29 +0545

    Axios and BASE_URL prototype

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.prototype.axios = axios
+Vue.prototype.BASE_URL = 'http://localhost:3000'
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
