d4725c53dc29766e27806af843c926dbf6a53064
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-27 12:22:44 +0545

    Error middeware

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -1,7 +1,9 @@
 function error(err, req, res, next) {
   if (process.env.NODE_ENV === 'production') {
-    return res.status(500).json({ message: err.message })
+    return res.status(err.statusCode || 500).json({ message: err.message })
   }
 
-  return res.status(500).json({ err, message: err.message })
+  res.status(err.statusCode || 500).json({ err, message: err.message })
 }
+
+module.exports = error
